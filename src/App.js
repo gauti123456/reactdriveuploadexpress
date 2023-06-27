466bfb58b82b2781d9a5f93ce90ab951fe3c7c2a
gauti123456 <geekygautam1997@gmail.com>
2023-06-27 17:05:00 +0530

    Initial commit - React.js app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
 
-function App() {
+const App = () => {
+  const fileInputRef = useRef(null);
+
+  const handleFileUpload = async () => {
+    const files = fileInputRef.current.files;
+
+    if (files.length > 0) {
+      const formData = new FormData();
+
+      for (let i = 0; i < files.length; i++) {
+        formData.append("files", files[i]);
+      }
+
+      try {
+        const response = await fetch("http://localhost:3001/upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        const data = await response.json();
+        console.log("Uploaded files:", data.files);
+      } catch (error) {
+        console.error("Error uploading files:", error);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload Files to Google Drive</h1>
+      <input type="file" multiple ref={fileInputRef} />
+      <button onClick={handleFileUpload}>Upload Files</button>
     </div>
   );
-}
+};
 
 export default App;
